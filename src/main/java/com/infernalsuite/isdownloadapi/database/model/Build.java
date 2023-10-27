46d1abd5d43f24ddb09bbbf35636caeb4e7c25de
Sam Turner <samt@thoriumcube.org>
2023-10-27 16:07:06 +0100

    Update Build

--- src/main/java/com/infernalsuite/isdownloadapi/database/model/Build.java
+++ src/main/java/com/infernalsuite/isdownloadapi/database/model/Build.java
@@ -1,13 +1,16 @@
 package com.infernalsuite.isdownloadapi.database.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import org.bson.types.ObjectId;
+import org.checkerframework.checker.nullness.qual.Nullable;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.CompoundIndex;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.Instant;
 import java.util.List;
+import java.util.Objects;
 
 @CompoundIndex(def = "{'project': 1, 'version': 1}")
 @CompoundIndex(def = "{'project': 1, 'version': 1, 'number': 1}")
@@ -18,8 +21,22 @@ public record Build(
         ObjectId version,
         int number,
         Instant time,
-        List<Change> changes
+        List<Change> changes,
+        @JsonProperty @Nullable Channel channel
         ) {
+
+    public Channel channelOrDefault() {
+        return Objects.requireNonNullElse(this.channel(), Channel.STABLE);
+    }
+
+    public enum Channel {
+        @JsonProperty("stable")
+        STABLE,
+        @JsonProperty("experimental")
+        EXPERIMENTAL,
+        @JsonProperty("pr")
+        PR;
+    }
 
     @Schema
     public record Change(
