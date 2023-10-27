9fefc65e0cf738ff3e19ba5efc420bd8c4e1fffe
Sam Turner <samt@thoriumcube.org>
2023-10-27 16:08:17 +0100

    Add tests

---
+++ src/test/java/com/infernalsuite/isdownloadapi/controller/v1/ProjectControllerTest.java
@@ -0,0 +1,89 @@
+package com.infernalsuite.isdownloadapi.controller.v1;
+
+import com.infernalsuite.isdownloadapi.ISDownloadApi;
+import com.infernalsuite.isdownloadapi.database.model.Project;
+import com.infernalsuite.isdownloadapi.database.model.Version;
+import com.infernalsuite.isdownloadapi.database.model.VersionFamily;
+import com.infernalsuite.isdownloadapi.database.repository.ProjectCollection;
+import com.infernalsuite.isdownloadapi.database.repository.VersionCollection;
+import com.infernalsuite.isdownloadapi.database.repository.VersionFamilyCollection;
+import org.bson.types.ObjectId;
+import org.json.JSONStringer;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.List;
+import java.util.Optional;
+
+//@WebMvcTest(value = ProjectController.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK, classes = ISDownloadApi.class)
+class ProjectControllerTest {
+
+//    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private ProjectCollection projectCollection;
+    @MockBean
+    private VersionFamilyCollection versionFamilyCollection;
+    @MockBean
+    private VersionCollection versionCollection;
+    @Autowired
+    WebApplicationContext webApplicationContext;
+
+    @BeforeEach
+    public void setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void givenListOfProjects_whenGetProjectsByName_returnProjectsByName() throws Exception {
+
+        ObjectId id1 = new ObjectId();
+
+        Project project1 = new Project(id1, "project1", "project1");
+
+        ObjectId versionFamilyId1 = new ObjectId();
+        VersionFamily versionFamily = new VersionFamily(versionFamilyId1, id1, "1.20", null);
+
+        ObjectId versionId1 = new ObjectId();
+        Version version = new Version(versionId1, id1, versionFamilyId1, "1.20.1", null);
+
+        Mockito.when(projectCollection.findByName("project1")).thenReturn(Optional.of(project1));
+        Mockito.when(versionFamilyCollection.findAllByProject(id1)).thenReturn(List.of(versionFamily));
+        Mockito.when(versionCollection.findAllByProject(id1)).thenReturn(List.of(version));
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/v1/projects/project", "project1").accept(MediaType.APPLICATION_JSON);
+
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+
+        String resultString = result.getResponse().getContentAsString();
+
+        JSONStringer expected = new JSONStringer();
+        expected.object()
+                .key("project_id").value("project1")
+                .key("project_name").value("project1")
+                .key("versions_group").array().value("1.20").endArray()
+                .key("versions").array().value("1.20.1").endArray()
+                .endObject();
+        System.out.println(expected);
+
+        String expectedString = expected.toString();
+
+        JSONAssert.assertEquals(expectedString, result.getResponse().getContentAsString(), true);
+
+
+    }
+
+}
---
+++ src/test/java/com/infernalsuite/isdownloadapi/controller/v1/ProjectsControllerTest.java
@@ -0,0 +1,61 @@
+package com.infernalsuite.isdownloadapi.controller.v1;
+
+import com.infernalsuite.isdownloadapi.database.model.Project;
+import com.infernalsuite.isdownloadapi.database.repository.ProjectCollection;
+import org.bson.types.ObjectId;
+import org.json.JSONStringer;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.List;
+
+@WebMvcTest(value = ProjectsController.class)
+class ProjectsControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private ProjectCollection projectCollection;
+
+    @Test
+    public void givenListOfProjects_whenGetProjects_returnProjects() throws Exception {
+
+        ObjectId id1 = new ObjectId();
+        ObjectId id2 = new ObjectId();
+
+        List<Project> projects = List.of(
+                new Project(id1, "project1", "project1"),
+                new Project(id2, "project2", "project2")
+        );
+
+        Mockito.when(projectCollection.findAll()).thenReturn(projects);
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/v1/projects").accept("application/json");
+
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+
+        String expected = "{projects:[project1,project2]}";
+
+        JSONAssert.assertEquals(expected, result.getResponse().getContentAsString(), false);
+
+        JSONStringer newexpected = new JSONStringer();
+        newexpected
+                .object()
+                .key("project_id").value("project1")
+                .key("project_name").value("project1")
+                .key("versions_group").array().value("1.20").endArray()
+                .key("versions").array().value("1.20.1").endArray()
+                .endObject();
+
+
+        System.out.println(newexpected.toString());
+    }
+}
