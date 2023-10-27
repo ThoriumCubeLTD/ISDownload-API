fe97923c5969a24b29928262363b94e95d291048
Sam Turner <samt@thoriumcube.org>
2023-10-27 16:07:30 +0100

    Add annotation to ArtifactCollection

--- src/main/java/com/infernalsuite/isdownloadapi/database/repository/ArtifactCollection.java
+++ src/main/java/com/infernalsuite/isdownloadapi/database/repository/ArtifactCollection.java
@@ -3,10 +3,12 @@ package com.infernalsuite.isdownloadapi.database.repository;
 import com.infernalsuite.isdownloadapi.database.model.Artifact;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface ArtifactCollection extends MongoRepository<Artifact, ObjectId> {
     List<Artifact> findAllByProjectAndVersionAndBuild(final ObjectId project, final ObjectId version, final ObjectId build);
     Optional<Artifact> findByProjectAndVersionAndBuildAndName(final ObjectId project, final ObjectId version, final ObjectId build, final String name);
