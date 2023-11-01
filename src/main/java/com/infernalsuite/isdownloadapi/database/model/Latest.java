f300a2c35255d43b302d485dd9d550fa274b56b1
Sam Turner <samt@thoriumcube.org>
2023-11-01 12:42:56 +0000

    Update Latest

--- src/main/java/com/infernalsuite/isdownloadapi/database/model/Latest.java
+++ src/main/java/com/infernalsuite/isdownloadapi/database/model/Latest.java
@@ -1,10 +1,13 @@
 package com.infernalsuite.isdownloadapi.database.model;
 
 import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "latest")
 public record Latest(
+        @Id
+        ObjectId id,
         ObjectId project,
         ObjectId version,
         ObjectId build
