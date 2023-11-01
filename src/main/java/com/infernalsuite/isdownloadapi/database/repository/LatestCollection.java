23924b3941cc4d9483b773ec4062b38dc6df8c38
Sam Turner <samt@thoriumcube.org>
2023-11-01 12:43:09 +0000

    Update LatestCollection

--- src/main/java/com/infernalsuite/isdownloadapi/database/repository/LatestCollection.java
+++ src/main/java/com/infernalsuite/isdownloadapi/database/repository/LatestCollection.java
@@ -2,8 +2,11 @@ package com.infernalsuite.isdownloadapi.database.repository;
 
 import com.infernalsuite.isdownloadapi.database.model.Latest;
 import org.bson.types.ObjectId;
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.checkerframework.checker.nullness.qual.Nullable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -11,4 +14,9 @@ import java.util.Optional;
 public interface LatestCollection extends MongoRepository<Latest, ObjectId> {
     Optional<Latest> findByProject(final ObjectId project);
     Optional<Latest> findByProjectAndVersion(final ObjectId project, final ObjectId version);
+    @Transactional
+    default void updateLatest(@Nullable ObjectId oldLatest, @NonNull Latest newLatest) {
+        if (oldLatest != null) this.deleteById(oldLatest);
+        this.save(newLatest);
+    }
 }
